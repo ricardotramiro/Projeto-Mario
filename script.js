63ca0f8a65d41bac0191f6e9a6e11d4e3eb315b4
ricardotramiro <ricardoteixeiraramiro@gmail.com>
2025-06-10 15:45:39 -0300

    Incluindo o JavaCcript

---
+++ script.js
@@ -0,0 +1,10 @@
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+
+function cliqueiNoBotao(){
+    form.style.left = "50%"
+    form.style.transform = "transletX(-50%)"
+    mascara.style.visibility = "visible"
+
+
+}
