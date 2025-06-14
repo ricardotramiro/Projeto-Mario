af0e082eb550172a58a728802f521a4a5e292b73
ricardotramiro <ricardoteixeiraramiro@gmail.com>
2025-06-14 10:45:37 -0300

    Alteração form.style.left para 42% pois assim fica no meio da tela

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const form = document.querySelector(".formulario-fale-conosco")
 const mascara = document.querySelector(".mascara-formulario")
 
 function cliqueiNoBotao(){
-    form.style.left = "50%"
+    form.style.left = "42%"
     form.style.transform = "transletX(-50%)"
     mascara.style.visibility = "visible"
 
