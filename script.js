47cd286f0962efc04cc7859424e30301bb6d60f1
ricardotramiro <ricardoteixeiraramiro@gmail.com>
2025-06-16 21:44:00 -0300

    Coreção translateX

--- script.js
+++ script.js
@@ -3,14 +3,18 @@ const mascara = document.querySelector(".mascara-formulario")
 
 // Função para chamar o formulario clicando em entre em contato//
 function mostrarFormulario(){
-    form.style.left = "42%"
-    form.style.transform = "transletX(-50%)"
+    form.style.left = "50%"
+    form.style.transform = "translateX(-50%)"
     mascara.style.visibility = "visible"
 }
 
 // Função para esconder o formulario clicando em qualquer campo da tela fora do formulario//
 function esconderFormulario(){
     form.style.left = "-300px"
-    form.style.transform = "transletX(0)"
+    form.style.transform = "translateX(0)"
     mascara.style.visibility = "hidden"
 }
+
+ 
+
+  
