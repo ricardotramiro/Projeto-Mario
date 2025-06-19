20a65ee75e519f31579097d1ba65740208607150
ricardotramiro <ricardoteixeiraramiro@gmail.com>
2025-06-19 17:57:42 -0300

    Add media screen para deixar o site responsivo

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
-const form = document.querySelector(".formulario-fale-conosco")
-const mascara = document.querySelector(".mascara-formulario")
+const form = document.querySelector(".formulario-fale-conosco");
+const mascara = document.querySelector(".mascara-formulario");
 
-// Função para chamar o formulario clicando em entre em contato//
-function mostrarFormulario(){
-    form.style.left = "50%"
-    form.style.transform = "translateX(-50%)"
-    mascara.style.visibility = "visible"
+// Mostrar o formulário
+function mostrarFormulario() {
+  form.style.left = "50%";
+  form.style.transform = "translateX(-50%)";
+  mascara.style.visibility = "visible";
 }
 
-// Função para esconder o formulario clicando em qualquer campo da tela fora do formulario//
-function esconderFormulario(){
-    form.style.left = "-300px"
-    form.style.transform = "translateX(0)"
-    mascara.style.visibility = "hidden"
+// Esconder o formulário
+function esconderFormulario() {
+  form.style.left = "-300px";
+  form.style.transform = "translateX(0)";
+  mascara.style.visibility = "hidden";
 }
 
-// quando clicar no botão ele volta ao topo da tela
- function voltarAoTopo() {
+// Voltar ao topo suavemente
+function voltarAoTopo() {
   window.scrollTo({
     top: 0,
     behavior: 'smooth'
   });
 }
 
-// Quando rolar o site pra baixo aparece o botão
+// Mostrar botão de voltar ao topo somente na seção
 window.addEventListener('scroll', function () {
   const botao = document.querySelector('.botao-voltar-topo');
   const secaoServicos = document.querySelector('.nossos-servicos');
   const posicaoSecao = secaoServicos.getBoundingClientRect();
 
-  // Se a parte superior da seção estiver visível na tela, mostra o botão
   if (posicaoSecao.top <= window.innerHeight && posicaoSecao.bottom >= 0) {
     botao.classList.add('mostrar');
   } else {
@@ -37,4 +36,3 @@ window.addEventListener('scroll', function () {
   }
 });
 
-  
